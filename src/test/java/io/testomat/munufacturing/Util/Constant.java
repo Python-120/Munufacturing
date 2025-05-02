a035fb32bd395e36d6ed7ecf2bb1542f29be77e7
denis <denis.murza@icloud.com>
2025-05-02 13:29:46 +0300

    test

--- src/test/java/io/testomat/munufacturing/Util/Constant.java
+++ src/test/java/io/testomat/munufacturing/Util/Constant.java
@@ -3,5 +3,6 @@ package io.testomat.munufacturing.Util;
 public class Constant {
     public final static String EMAIL = "testomat@gmail.com";
     public final static String PASSWORD = "testomat";
+    String string;
 
 }
